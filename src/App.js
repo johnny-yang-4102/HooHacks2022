90610a624b97e631782dd61fd126be1c5bdb4652
allen <allendoelue@gmail.com>
2022-03-26 17:31:13 -0400

    Worked on score functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import Person from "./components/Person";
 function App() {
+
+  const personObj = {
+    firstName: "Allen",
+    lastName: "Doelue",
+    Age: 21,
+    Gender: "Male",
+    profilePic: "",
+    id: 1,
+    workoutGoal: "Muscle Mass",
+    availDaysOfWeek: [1, 0, 1, 0, 1, 0, 0], //Monday - Sunday
+    squat: 225,
+    deadlift: 225,
+    bench: 145,
+    expRating: 5
+
+  }
 
   return (
     <div className="App">
-      <p>hello world</p>
+      <>
+      <Person person={personObj}/>
+      <p></p>
+      </>
+        
     </div>
   );
 }
--- src/components/Person.js
+++ src/components/Person.js
@@ -12,11 +12,36 @@ const Person = ({ person }) => {
 
         //cVal = compatibility value
             //Obtain person attributes and comparePerson attributes and compare them
-        const cvalWorkoutGoal = 0
-        const cvalAvailDaysOfWeek = 0
+        const cvalWorkoutGoal = 0  //30%
+        const cvalAvailDaysOfWeek = 0 //10%
+        const cvalSquat = 0 //5%
+        const cvalDeadlift = 0 //5%
+        const cvalBench = 0 //10%
+        const cvalExpRating = 0 // 30%
+
+      
+        return person.expRating
+
+        
+
 
     }
 
+
+    const personObj = {
+        firstName: "Johnny",
+        lastName: "Yang",
+        Age: 20,
+        Gender: "Male",
+        profilePic: "",
+        id: 1,
+        workoutGoal: "Muscle Mass",
+        availDaysOfWeek: [1, 0, 1, 0, 1, 0, 0], //Monday - Sunday
+        squat: 225,
+        deadlift: 225,
+        bench: 145,
+        expRating: 5
+      }
 
     //Between the div elements, put in html elements with component's attributes so we can make sure it's working.
         //the name of attributes are on google doc
@@ -26,14 +51,12 @@ const Person = ({ person }) => {
         <div>
             <p>{person.firstName}</p>
             <p>{person.lastName}</p>
+            <p>{person.Age}</p>
+            {getCompatibilityScore(personObj)}
         </div>
     )
 
 
 }
-
-
-
-
 
 export default Person
